723d3993ae4b7c0770bcfd64fa2c5603e3ed2da0
Lucas Costamagna <149833176+Lucmania@users.noreply.github.com>
2024-11-13 17:55:49 -0300

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '3534772356Lucas*',
+    password: 'Password here!!',
     database: 'soderia'
 });
 
